8d4663be310887af4f733ae19d08caceaa8bf850
James Flynn <james-flynn-ie@users.noreply.github.com>
2023-08-16 19:21:23 +0100

    Added app.py and init

---
+++ hello-rest-api/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/api/v1/hello", methods=['GET'])
+def hello():
+    msg = {
+        "msg": "hello"
+    }
+    return (jsonify(msg), 200)
+
+@app.route("/api/v1/handshake", methods=['POST'])
+def handshake():
+    msg = {
+        "msg": "handshake"
+    }
+    return (jsonify(msg), 200)
+
+
+if __name__ == "__main__":
+    app.run()
