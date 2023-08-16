dcd0b5dee2b7ab4a506ebd2c1bdadf6d0054c950
James Flynn <james-flynn-ie@users.noreply.github.com>
2023-08-16 20:41:20 +0100

    Feature/GitHub workflow (#4)
    * Added Github workflow
    
    * Rename hello_rest_api for mypy convention;fix lint
    
    * Added test-requirements.txt for pipeline
    
    * fixed pylint warnings
    
    * refactored linting workflow
    
    * refactored linting workflow
    
    * updated pytest path in workflow

--- hello_rest_api/app.py
+++ hello_rest_api/app.py
@@ -1,16 +1,23 @@
+"""
+flask web server.
+"""
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/api/v1/hello", methods=['GET'])
 def hello():
+    """returns 200 OK and JSON {msg": "hello"}"""
     msg = {
         "msg": "hello"
     }
     return (jsonify(msg), 200)
 
+
 @app.route("/api/v1/handshake", methods=['POST'])
 def handshake():
+    """returns 200 OK and JSON {msg": "handshake"}"""
     msg = {
         "msg": "handshake"
     }
--- hello_rest_api/test_app.py
+++ hello_rest_api/test_app.py
@@ -1,14 +1,21 @@
 # test_app.py
-import app
+"""
+Tests hello_rest_api endpoints.
+"""
+from . import app
+
 
 def test_hello():
+    """expects 200 OK and JSON {msg": "hello"}"""
     web = app.app.test_client()
 
     response = web.get('/api/v1/hello')
     assert response.status == '200 OK'
     assert response.data == b'{"msg":"hello"}\n'
 
+
 def test_handshake():
+    """expects 200 OK and JSON {msg": "handshake"}"""
     web = app.app.test_client()
 
     response = web.post('/api/v1/handshake')
