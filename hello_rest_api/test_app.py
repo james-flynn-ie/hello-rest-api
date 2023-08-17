cc9d07827d9effed6cbd75882f7440bbd2486221
James Flynn <james-flynn-ie@users.noreply.github.com>
2023-08-17 19:33:22 +0100

    Added unit test for round robin feature

--- hello_rest_api/test_app.py
+++ hello_rest_api/test_app.py
@@ -14,6 +14,26 @@ def test_hello():
     assert response.data == b'{"msg":"hello"}\n'
 
 
+def test_hello_round_robin():
+    """
+    Test the round robin feature.
+    Expects 200 OK and that the JSON message will alternate for each run
+    """
+    web = app.app.test_client()
+
+    response = web.get('/api/v1/hello')
+    assert response.status == '200 OK'
+    assert response.data == b'{"msg":"Hello from new v1"}\n'
+
+    response = web.get('/api/v1/hello')
+    assert response.status == '200 OK'
+    assert response.data == b'{"msg":"hello"}\n'
+
+    response = web.get('/api/v1/hello')
+    assert response.status == '200 OK'
+    assert response.data == b'{"msg":"Hello from new v1"}\n'
+
+
 def test_handshake():
     """expects 200 OK and JSON {msg": "handshake"}"""
     web = app.app.test_client()
