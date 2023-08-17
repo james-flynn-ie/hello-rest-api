f72fcf2fe54290bf793d7859cd8ff3bd1781398d
James Flynn <james-flynn-ie@users.noreply.github.com>
2023-08-17 19:47:47 +0100

    Add round robin response class for hello endpoint (#6)
    * Added RoundRobin class for hello endpoint
    
    * Added RoundRobin class for hello endpoint
    
    * Added unit test for round robin feature
    
    * Updated README to document round-robin feature
    
    * Refactored comment
    
    * Updated pipeline to also run for pull requests
    
    * Updated PR template to remove unused section

--- hello_rest_api/app.py
+++ hello_rest_api/app.py
@@ -6,18 +6,56 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
+class RoundRobinResponse:
+    """
+    Selects the response for the hello endpoint in round-robin format,
+    where 50% of the responses return "hello"
+    and 50% return "Hello from new v1".
+    """
+    def __init__(self, responses):
+        self.responses = responses
+        self.current_response_index = 0
+
+    def get_next_response(self):
+        """
+        Get the next round-robin response.
+        The value returned is the list item at the current response index.
+        """
+        response = self.responses[self.current_response_index]
+        self.increment_index()
+        return response
+
+    def increment_index(self):
+        """
+        Increment the current_response_index,
+        using the mod of the responses list length to stay in bounds.
+        """
+        self.current_response_index = ((self.current_response_index + 1)
+                                       % len(self.responses))
+
+
+hello_responses = [
+    "hello",
+    "Hello from new v1",
+]
+
+round_robin = RoundRobinResponse(hello_responses)
+
+
 @app.route("/api/v1/hello", methods=['GET'])
 def hello():
-    """returns 200 OK and JSON {msg": "hello"}"""
+    """returns 200 OK and JSON message"""
+    response = round_robin.get_next_response()
+
     msg = {
-        "msg": "hello"
+        "msg": f"{response}"
     }
     return (jsonify(msg), 200)
 
 
 @app.route("/api/v1/handshake", methods=['POST'])
 def handshake():
-    """returns 200 OK and JSON {msg": "handshake"}"""
+    """returns 200 OK and JSON {"msg": "handshake"}"""
     msg = {
         "msg": "handshake"
     }
